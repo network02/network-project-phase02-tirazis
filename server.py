fcd1831b83830ac3f9f8b22ae48cf8ee9af2f078
Aliakbar Ahrari <ahr04624@gmail.com>
2024-01-09 01:29:12 +0330

    handling accessibility added

--- server.py
+++ server.py
@@ -4,23 +4,20 @@ import sqlite3
 import threading
 import time
 
-users = {
-    'user1': {'username': 'Mehdi', 'password': '1234'},
-    'user2': {'username': 'Bob', 'password': '25'},
-    'user3': {'username': 'John', 'password': '35'},
-}
-
 commands_list = [
-    ("LIST", "List contents of a directory: LIST <path>"),
-    ("RETR", "Retrieve a file: RETR <filename>"),
-    ("STOR", "Store a file: STOR <filename>"),
-    ("DELE", "Delete a file: DELE <filename>"),
-    ("MKD", "Make a directory: MKD <directory name>"),
-    ("RMD", "Remove a directory: RMD <directory name>"),
-    ("PWD", "Print working directory: PWD"),
-    ("CWD", "Change working directory: CWD <directory name>"),
-    ("CDUP", "Change to the parent directory: CDUP"),
-    ("QUIT", "Quit the FTP session: QUIT")
+    ("LOGIN\t", "Login to the FTP server: LOGIN <username> <password>"),
+    ("SIGNUP\t", "Create a new user: SIGNUP <username> <password>"),
+    ("LIST\t", "List contents of a directory: LIST <path>"),
+    ("RETR\t", "Retrieve a file: RETR <filename>"),
+    ("STOR\t", "Store a file: STOR <filename>"),
+    ("DELE\t", "Delete a file: DELE <filename>"),
+    ("MKD\t", "Make a directory: MKD <directory name>"),
+    ("RMD\t", "Remove a directory: RMD <directory name>"),
+    ("PWD\t", "Print working directory: PWD"),
+    ("CWD\t", "Change working directory: CWD <directory name>"),
+    ("CDUP\t", "Change to the parent directory: CDUP"),
+    ("QUIT\t", "Quit the FTP session: QUIT"),
+    ("REPORT\t", "(Only for admin) Generate a server report: REPORT")
 ]
 
 
@@ -51,13 +48,48 @@ def authenticate_user(username, password):
         return False
 
 
+def check_access(filename, username):
+    try:
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT accessibility, authorized_users FROM user_permissions WHERE filename=?", (filename,))
+        access_data = cursor.fetchone()
+
+        if access_data:
+            accessibility, authorized_users = access_data
+            if accessibility == 'public':
+                return True
+            elif accessibility == 'private' and authorized_users:
+                authorized_users = authorized_users.split(',')  # Convert to list
+                if username in authorized_users:
+                    return True
+
+        return False
+
+    except Exception as e:
+        error_msg = f"An error occurred while checking access: {e}"
+        print(error_msg)
+        return False
+
+
 class FTPthread(threading.Thread):
+    access_control = {
+        "private_file.txt": ["Reza", "Ali"],
+        "private_directory": ["user1"],
+        # Add more entries as needed
+    }
+
     def __init__(self, client, clientaddress, localip, dataport):
+        self.log_messages = ''
         self.client = client
         self.client_address = clientaddress
         self.data_address = (localip, dataport)
         self.cwd = os.getcwd
         threading.Thread.__init__(self)
+
+        self.conn = sqlite3.connect('users.db')  # Connect to the database
+        self.cursor = self.conn.cursor()
 
     def run(self):
         # Sending the welcome message to the client
@@ -65,7 +97,7 @@ class FTPthread(threading.Thread):
         if cmd.decode() == 'FIRST':
             welcome_message = "\n... < Welcome to the FTP server > ...\n(!) Available commands:\n"
             for command, description in commands_list:
-                welcome_message += f"\t{command}: {description}\n"
+                welcome_message += f"\t{command}-->\t{description}\n"
             self.client.send(welcome_message.encode())
         while True:
             try:
@@ -75,21 +107,27 @@ class FTPthread(threading.Thread):
                 user_command = user_str_cmds[0].upper()
                 username = user_str_cmds[1]
             except:
-                self.client.send('test1'.encode())
-                continue
+                print('USER QUIT')
+                break
+
             # Handle USER-Login
-            if user_command == 'USER':
+            if user_command == 'LOGIN':
+                if user_str_cmds[1] == 'admin' and user_str_cmds[2] == 'admin':
+                    self.client.send("230 Logged in successfully\r\n".encode())
+                    self.switches(2, username)
+                    break
                 try:
                     password = user_str_cmds[2]
                     if authenticate_user(username, password):
                         self.client.send("230 Logged in successfully\r\n".encode())
-                        self.switches()
+                        print(f'(!) USER \"{username}\" logged-in to server')
+                        self.switches(1, username)
                         break
                     else:
                         self.client.send("530 Login incorrect\r\n".encode())
                         break
                 except:
-                    self.client.send("500 Syntax error, command unrecognized\r\n".encode())
+                    print(f"(!) USER \"{username}\" leaved from server")
                     break
                     # Handle SIGNUP
             elif user_command == 'SIGNUP':
@@ -99,6 +137,7 @@ class FTPthread(threading.Thread):
 
                     create_user(username, password)
                     self.client.send("201 User created successfully\r\n".encode())
+                    print(f'(!) USER \"{username}\" Signed-up in server')
                 except:
                     self.client.send("500 Syntax error, command unrecognized\r\n".encode())
                     break
@@ -109,47 +148,76 @@ class FTPthread(threading.Thread):
                 self.client.send("500 Syntax error, command unrecognized\r\n".encode())
         self.client.close()
 
-    def switches(self):
+    def switches(self, roll, username):
         while True:
 
             command = self.client.recv(1024).decode().strip()
-            # Show the server items
-            if command.startswith('LIST'):
-                pathname = ''
-                parts = command.split(' ')
-                if len(parts) > 1:
-                    pathname = parts[1]
-                self.LIST(pathname)
-                continue
-            # Download a file from server
-            elif command.startswith('RETR'):
-                filename = command.split(' ')[1]
-                self.RETR(filename)
-                continue
-            # Upload a file to server
-            elif command.startswith('STOR'):
-                filename = command.split(' ')[1]
-                self.RETR(filename)
-                continue
-            # Delete a file in server
-            elif command.startswith('DELE'):
-                filename = command.split(' ')[1]
-                self.DELE(filename)
-                continue
-            # Create a new directory
-            elif command.startswith('MKD'):
-                dir_name = command.split(' ')[1]
-                self.MKD(dir_name)
-                continue
-            # Returns the current working directory
-            elif command.startswith('PWD'):
-                self.PWD()
-                continue
-            #
-            elif command.startswith('RMD'):
-                dir_name = command.split(' ')[1]
-                self.RMD(dir_name)
-                continue
+            try:
+                # Show the server items
+                if command.startswith('LIST'):
+                    pathname = ''
+                    parts = command.split(' ')
+                    if len(parts) > 1:
+                        pathname = parts[1]
+                    self.LIST(pathname)
+                    continue
+                # Download a file from server
+                elif command.startswith('RETR'):
+                    filename = command.split(' ')[1]
+                    if check_access(filename, username):
+                        self.RETR(filename)
+                        continue
+                    else:
+                        self.client.send("550 Permission denied\r\n".encode())
+                    continue
+                # Upload a file to server
+                elif command.startswith('STOR'):
+                    filename = command.split(' ')[1]
+                    if check_access(filename, username):
+                        self.RETR(filename)
+                        continue
+                    else:
+                        self.client.send("550 Permission denied\r\n".encode())
+                    continue
+
+                # Delete a file in server
+                elif command.startswith('DELE'):
+                    filename = command.split(' ')[1]
+                    if check_access(filename, username):
+                        self.DELE(filename)
+                        continue
+                    else:
+                        self.client.send("550 Permission denied\r\n".encode())
+                    continue
+                # Create a new directory
+                elif command.startswith('MKD'):
+                    dir_name = command.split(' ')[1]
+                    self.MKD(dir_name)
+                    continue
+                # Returns the current working directory
+                elif command.startswith('PWD'):
+                    self.PWD()
+                    continue
+                #
+                elif command.startswith('RMD'):
+                    dir_name = command.split(' ')[1]
+                    self.RMD(dir_name)
+                    continue
+                elif command.startswith('REPORT') and roll == 2:
+                    print('admin logged in')
+                    self.REPORT()
+                    continue
+                else:
+                    self.client.send('(!) Command unrecognized! Try a different command.'.encode())
+                    continue
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+    def __del__(self):
+        try:
+            self.conn.close()  # Close the database connection when the thread is destroyed
+        except Exception as e:
+            error_msg = f"An error occurred while closing the database connection: {e}"
 
     def LIST(self, pathname=''):
         try:
@@ -214,14 +282,9 @@ class FTPthread(threading.Thread):
 
     def DELE(self, filename):
         try:
-            self.client.send(f"250 Do you really wish to delete '{filename}'? Y/N\r\n".encode())
-            response = self.client.recv(1024).decode().strip().upper()
+            os.remove(filename)
+            self.client.send(f"250 File '{filename}' deleted successfully\r\n".encode())
 
-            if response == 'Y':
-                os.remove(filename)
-                self.client.send(f"250 File '{filename}' deleted successfully\r\n".encode())
-            else:
-                self.client.send(f"250 Operation canceled by user\r\n".encode())
         except FileNotFoundError:
             self.client.send(f"550 File '{filename}' not found\r\n".encode())
         except Exception as e:
@@ -261,6 +324,10 @@ class FTPthread(threading.Thread):
         except:
             self.client.send(f"550 Directory '{dir_name}' cant be removed\r\n".encode())
 
+    def REPORT(self):
+        respond = os.getcwd()
+        self.client.send(f"257 Current working directory is '{respond}'\r\n".encode())
+
 
 class FTP_server:
     def __init__(self):
@@ -282,6 +349,26 @@ class FTP_server:
             quit()
 
     def startt(self):
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        cursor.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT,
+                    password TEXT
+                )
+            ''')
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS user_permissions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                filename TEXT,
+                accessibility TEXT,
+                authorized_users TEXT
+            )
+        ''')
+
         print('...< FTP SERVER STARTED >...')
         self.start_socket()
 
@@ -298,18 +385,8 @@ class FTP_server:
             self.sock.close()
             quit()
 
+    # Create the SQLite database and table if they don't exist
 
-# Create the SQLite database and table if they don't exist
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT,
-        password TEXT
-    )
-''')
 
 ftp_server = FTP_server()
 ftp_server.startt()
